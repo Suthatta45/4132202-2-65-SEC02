80cfc9b94dcd85214e671e49a04ba150ceec48fa
Suthatta45 <640112418084@bru.ac.th>
2022-12-08 12:12:34 +0700

    LAB4

---
+++ LAB4/php/model/condb.php
@@ -0,0 +1,14 @@
+<?php
+$host = "db";    //db service from docker-compose
+$user = "root";
+$pass = "1234";
+$db = "db_STUDENT";
+
+try{
+    $link = mysqli_connect($host,$user,$pass,$db);
+    mysqli_query($link, "SET NAMES utf8");
+} catch (Exception $e){
+    echo "An error is" . $e . " :: " . mysqli_error($link);
+}
+
+?>
